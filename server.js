e6726e92e0dffaf1ed9ddc53c2d01be25efdd01c
HWUN <harvey.wun@gmail.com>
2017-03-12 07:50:01 +0000

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var path = require('path');
+var upload = multer({
+  dest: './uploads/'
 });
 
-app.listen(process.env.PORT || 8080, function () {
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+app.post('/', upload.single('upl'), function (req, res, next) {
+  res.send(JSON.stringify({size: req.file.size}));
+  //res.status(204).end();
+});
+
+
+app.listen(process.env.PORT || 8080, function() {
   console.log('Example app listening');
-})
+});
